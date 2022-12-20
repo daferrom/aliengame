6175363653e89841992f191b22cfa431266452e5
Diego Ferro <daferrom@gmail.com>
2022-12-20 09:36:43 -0500

    WIP: Added modal losing game  and SVG face img

--- src/components/GameStateFinish.vue
+++ src/components/GameStateFinish.vue
@@ -2,12 +2,15 @@
   <div class="modal">
     <h1 id="header">You Won!</h1>
     <p>You Won!</p>
+    <img src="../assets/face.svg" />
     <button>Let's play again!</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "GameStateFinish",
+};
 </script>
 
 <style lang="scss" scoped></style>
